f29d3f82acb5fd56dcbd7a6ca5179a78d91a7423
EdersonSouza02 <edersonfreitas2012@hotmail.com>
2020-09-23 21:25:55 -0300

    Realizando cadastrar de usuario

--- laflente_backoffice/src/main/java/com/xnexus/controller/model/EntradaProduto.java
+++ laflente_backoffice/src/main/java/com/xnexus/controller/model/EntradaProduto.java
@@ -7,9 +7,7 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
-import javax.validation.constraints.NotEmpty;
-import javax.validation.constraints.NotNull;
-import javax.validation.constraints.Size;
+
 
 @Entity
 public class EntradaProduto implements Serializable {
