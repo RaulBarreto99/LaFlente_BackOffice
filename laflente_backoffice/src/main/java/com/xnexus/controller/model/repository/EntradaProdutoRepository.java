86529dbd1b4747e26124cf65346732da32084deb
Raul <raul_barreto99@live.com>
2020-10-05 23:44:21 -0300

    perguntas e respostas backoffice concluido com sucesso

--- laflente_backoffice/src/main/java/com/xnexus/controller/model/repository/EntradaProdutoRepository.java
+++
@@ -1,9 +0,0 @@
-package com.xnexus.controller.model.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.xnexus.controller.model.EntradaProduto;
-
-public interface EntradaProdutoRepository extends JpaRepository<EntradaProduto, Long> {
-
-}
